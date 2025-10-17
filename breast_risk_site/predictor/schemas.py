c149e1af0275716ca68c74cba848142b2f2e515a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-10-17 10:48:19 +0100

    Add initial schemas and constants for form and view validation

---
+++ breast_risk_site/predictor/schemas.py
@@ -0,0 +1,5 @@
+"""Schemas and constants used by forms and views."""
+
+MAG_CHOICES = [("40x", "40x"), ("100x", "100x"), ("unknown", "Unknown")]
+HRT_CHOICES = [(0, "Never"), (1, "Past"), (2, "Current")]
+SMOKE_CHOICES = [(0, "Never"), (1, "Former"), (2, "Current")]
