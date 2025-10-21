0421f271726ab53cc01686629ba2e5a1a94ded5d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-10-21 15:15:16 +0100

    Add initial database models placeholder for future SQLAlchemy integration

---
+++ Breast-Cancer-Risk-Prediction-Project/predictor/models.py
@@ -0,0 +1,13 @@
+"""Database models placeholder.
+
+If you add SQLAlchemy, define your declarative models here. Kept empty for now.
+"""
+# from .extensions import db
+#
+# class ExampleModel(db.Model):
+#     __tablename__ = "example"
+#     id = db.Column(db.Integer, primary_key=True)
+#     created_at = db.Column(db.DateTime, default=db.func.now())
+#
+#     def __repr__(self):
+#         return f"<ExampleModel id={self.id}>"
